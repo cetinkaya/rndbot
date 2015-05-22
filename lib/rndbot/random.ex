4efe0963422578cb2e54df81ffec0cefc7c404e8
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2015-05-23 02:28:53 +0900

    Add copyleft notice.

--- lib/rndbot/random.ex
+++ lib/rndbot/random.ex
@@ -1,3 +1,20 @@
+# Copyright 2015 Ahmet Cetinkaya
+
+# This file is part of RndBot.
+
+# RndBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# RndBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with RndBot.  If not, see <http://www.gnu.org/licenses/>.
+
 defmodule RndBot.Random do
   def rand(min, max) when min < max and min >= 1 and max <= 10000 do
     url = "https://www.random.org/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new"
