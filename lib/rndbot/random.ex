d3a1716f901be27d54b8e99ba5ee77761d0a080b
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2015-05-23 01:44:53 +0900

    Add limits to min and max values.

--- lib/rndbot/random.ex
+++ lib/rndbot/random.ex
@@ -1,5 +1,5 @@
 defmodule RndBot.Random do
-  def rand(min, max) when min < max do
+  def rand(min, max) when min < max and min >= 1 and max <= 10000 do
     url = "https://www.random.org/integers/?num=1&min=#{min}&max=#{max}&col=1&base=10&format=plain&rnd=new"
     :hackney.start
     number = case :hackney.request("get", to_char_list(url), [], '', []) do
